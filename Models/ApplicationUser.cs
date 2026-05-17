65e2189a82ff2febd943a0f9dba1db5797b24b8f
Bjoniii <bjoni@motomtech.com>
2026-05-17 21:17:29 +0200

    extend user model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName {get;set;}
+    public string Address { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
